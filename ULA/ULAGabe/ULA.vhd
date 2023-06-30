6a1294a742974c16c8f53fcceb0a57144c7f495f
Radajaaj <1vitor.camargo@gmail.com>
2023-06-30 14:29:30 -0300

    Reduzimos o numero de signals na ULA.vhd

--- ULA/ULAGabe/ULA.vhd
+++ ULA/ULAGabe/ULA.vhd
@@ -12,20 +12,20 @@ end entity ;
 
 architecture arch of ULA is
   component mux5x8 is port(
-      A : in std_logic_vector (7 downto 0); --NOT
-      B : in std_logic_vector (7 downto 0);--AND
-      C : in std_logic_vector (7 downto 0);--OR
-      D : in std_logic_vector (7 downto 0);--ADD
-      E : in std_logic_vector (7 downto 0);--LDA
+      A   : in std_logic_vector (7 downto 0); --NOT
+      B   : in std_logic_vector (7 downto 0);--AND
+      C   : in std_logic_vector (7 downto 0);--OR
+      D   : in std_logic_vector (7 downto 0);--ADD
+      E   : in std_logic_vector (7 downto 0);--LDA
       sel : in std_logic_vector (2 downto 0);--ULA_op
-      H : out std_logic_vector(7 downto 0)--s_resultado
+      H   : out std_logic_vector(7 downto 0)--s_resultado
   );
   end component;
 
   component Detector_NZ is
     port (
       datain : in std_logic_vector(7 downto 0);
-      dataout : out std_logic_vector(1 downto 0)--N e Z
+      FNZ : out std_logic_vector(1 downto 0)--N e Z
     ); 
   end component;
 
@@ -39,22 +39,16 @@ architecture arch of ULA is
     );
   end component;
 
-  signal sinal_not, sinal_add, sinal_and, sinal_or, sinal_lda, out_ula : std_logic_vector(7 downto 0);
-  signal sinal_dataout : std_logic_vector(1 downto 0);
-  
+  signal sinal_not, sinal_add, sinal_and, sinal_or : std_logic_vector(7 downto 0);
   
 begin
 
   sinal_and <= X and Y;
   sinal_not <= not(X); 
   sinal_or <= X or Y;
-  sinal_lda <= Y;
-  
-  Saida_ULA <= out_ula;
   
   U_soma : somador8bit port map (X, Y, '0', sinal_add);
-  U_ULA : mux5x8 port map (sinal_not, sinal_and, sinal_or, sinal_add, sinal_lda, ULA_op, Saida_ULA);
-  U_detc : Detector_NZ port map (out_ula, sinal_dataout );
+  U_ULA : mux5x8 port map (sinal_not, sinal_and, sinal_or, sinal_add, Y, ULA_op, Saida_ULA);
+  U_detc : Detector_NZ port map (Saida_ULA, NZ);
   
-
 end architecture ;
