31c941c62f0c6974927df960c0956e665d001af5
Radajaaj <1vitor.camargo@gmail.com>
2023-07-12 09:34:21 -0300

    Mais comentarios no decode

--- CONTROLE/Decode.vhdl
+++ CONTROLE/Decode.vhdl
@@ -11,15 +11,15 @@ end decodificador;
 architecture jorge of decodificador is 
 begin
     
-   instrout <=  "10000000000" when instrin = "00000000" else -- snop (nenhuma operacao)
-                "01000000000" when instrin = "00010000" else -- ssta (armazena acumulador - store)
-                "00100000000" when instrin = "00100000" else -- slda (carrega acumulador  - load)
-                "00010000000" when instrin = "00110000" else -- sadd (soma)
-                "00001000000" when instrin = "01000000" else -- sor  ("ou" logico)
-                "00000100000" when instrin = "01010000" else -- sand ("e" logico)
-                "00000010000" when instrin = "01100000" else -- snot (inverte (complementa) acumulador)
-                "00000001000" when instrin = "10000000" else -- sjmp (desvio incondicional - jump)
-                "00000000100" when instrin = "10010000" else -- sjn  (desvio condicional   - jump on negative)
-                "00000000010" when instrin = "10100000" else -- sjz  (desvio condicional   - jump on zero)
-                "00000000001" when instrin = "11110000";     -- shlt (termino de execucao  - halt)
+   instrout <=  "10000000000" when instrin = "00000000" else -- snop (nenhuma operacao)             --
+                "01000000000" when instrin = "00010000" else -- MEM(end) <- AC          ssta (armazena acumulador)
+                "00100000000" when instrin = "00100000" else -- AC <- MEM(end)          slda (carrega acumulador)
+                "00010000000" when instrin = "00110000" else -- AC <- MEM(end) + AC     sadd (soma)
+                "00001000000" when instrin = "01000000" else -- AC <- MEM(end) or AC    or("ou")
+                "00000100000" when instrin = "01010000" else -- AC <- MEM(end) and AC   sand ("e")
+                "00000010000" when instrin = "01100000" else -- AC <- not(AC)           snot (inverte acumulador)
+                "00000001000" when instrin = "10000000" else -- PC <- end               sjmp (desvio incondicional - jump)
+                "00000000100" when instrin = "10010000" else -- if(N=1) then PC <- end  sjn  (desvio condicional - jump on negative)
+                "00000000010" when instrin = "10100000" else -- if(Z=1) then PC <- end  sjz  (desvio condicional - jump on zero)
+                "00000000001" when instrin = "11110000";     -- shlt (termino de execucao - halt)
 end jorge;
